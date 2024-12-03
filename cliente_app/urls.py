a927888430d1c59a2aced11af1f40f6b598e3ee6
Miriam-BonillaZ-1050 <167555012+Miriam-BonillaZ-1050@users.noreply.github.com>
2024-12-03 12:55:22 -0600

    Add files via upload

---
+++ cliente_app/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path 
+from cliente_app import views
+urlpatterns = [
+    path('cliente', views.inicio_vistaCliente, name="cliente"),
+    path('registrarCliente/',views.registrarCliente,name="registrarCliente"),
+    path('seleccionarCliente/<id_cliente>',views.seleccionarCliente,name="seleccionarCliente"),
+    path('editarCliente/',views.editarCliente,name="editarCliente"),
+    path('borrarCliente/<id_cliente>',views.borrarCliente,name="borrarCliente"),
+]
